2bd5f9c12e529c7b4c039f613a3af3b476391b03
Jarquevious <jarquevious@gmail.com>
2021-03-01 23:42:53 -0800

    added base case for 0

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 """Function to raise power recursively"""
 def raise_power(base,exp):
 
-    """Base Case"""
-    """Checks to see if Case exp==1"""
+    """Base Cases"""
+    """Checks to see if exp==0"""
+    if(exp == 0):
+        return 1
+
+    """Checks to see if exp==1"""
     if(exp == 1):
-        """Returns base num raise to is num itself"""
+        """Returns base of any num raise to 1 equals num itself"""
         return(base)
 
     """Recursive Case"""
     """Checks to see if exp is not equal to one"""
-    elif(exp != 1):
-        
+    if(exp != 1):
+
         """Mulitiply base recursively by the function"""
         return(base * raise_power(base, exp-1))
 
+"""Prints result after function is complete"""
+print(raise_power(8,2))
 
-print(raise_power(20,2))
 
